8cf2467ce2744b33c31c5329265a95978301666c
QIanZhang1218 <zhang_qianzqzq@163.com>
2022-04-05 20:24:14 +1200

    Minor Change

--- src/App.js
+++ src/App.js
@@ -2,48 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/navbar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Header {...header} />
+      <Header />
     </div>
   );
-}
-var header = {};
-header.brand = {
-  linkTo: "#",
-  text: "React Bootstrap Logi",
 };
-header.links = [
-  {
-    linkTo: "#",
-    text: "Classes",
-  },
-  {
-    linkTo: "#",
-    text: "Lessons",
-  },
-  {
-    linkTo: "#",
-    text: "Libraries",
-  },
-  {
-    dropdown: true,
-    text: "teacher@school.org",
-    links: [
-      {
-        linkTo: "#",
-        text: "student@school.org",
-      },
-      {
-        linkTo: "#",
-        text: "Settings",
-      },
-      {
-        linkTo: "#",
-        text: "Sign Out",
-      },
-    ],
-  },
-];
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,24 +25,22 @@ const Header = () => {
   );
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="%" className="ms-3">
-          <img
-            src="../../logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="{this.props.brand.text}"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          {isStudent ? studentNav : teacherNav}
-          <Dropdown isStudent={isStudent} setStudent={changeRole} />
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="%" className="ms-3">
+        <img
+          src="../../logo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="{this.props.brand.text}"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {isStudent ? studentNav : teacherNav}
+        <Dropdown isStudent={isStudent} setStudent={changeRole} />
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 export default Header;
